a2e4f64c4eb2ff92fe09510b47b865d9138e17e7
isaacs <i@foohack.com>
2008-12-24 13:14:28 -0800

    (I know bash, I swear.)

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 ! [ -d out ] && mkdir out
-[ "`which php 2>/dev/null`" ] && php src/goosh.js > ./out/goosh.raw.js
-[ "`which yuicompressor 2>/dev/null`" ] && yuicompressor ./out/goosh.raw.js > ./out/goosh.js
-[ "`which gzip 2>/dev/null`" ] && gzip < ./out/goosh.js > ./out/goosh.js.gz
+[ -f "`which php 2>/dev/null`" ] && php src/goosh.js > ./out/goosh.raw.js
+[ -f "`which yuicompressor 2>/dev/null`" ] && yuicompressor ./out/goosh.raw.js > ./out/goosh.js
+[ -f "`which gzip 2>/dev/null`" ] && gzip < ./out/goosh.js > ./out/goosh.js.gz
