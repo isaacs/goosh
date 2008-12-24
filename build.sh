d2c0ecf79805999178959a30a13cce5966cd84df
isaacs <i@foohack.com>
2008-12-24 13:13:38 -0800

    Add capability testing.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 ! [ -d out ] && mkdir out
-php src/goosh.js > ./out/goosh.raw.js
-yuicompressor ./out/goosh.raw.js > ./out/goosh.js
-gzip < ./out/goosh.js > ./out/goosh.js.gz
+[ "`which php 2>/dev/null`" ] && php src/goosh.js > ./out/goosh.raw.js
+[ "`which yuicompressor 2>/dev/null`" ] && yuicompressor ./out/goosh.raw.js > ./out/goosh.js
+[ "`which gzip 2>/dev/null`" ] && gzip < ./out/goosh.js > ./out/goosh.js.gz
